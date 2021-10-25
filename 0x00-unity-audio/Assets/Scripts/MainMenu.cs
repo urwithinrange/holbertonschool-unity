b9aa1f94edac7bcfa673bb94d154f1d10b1e8db6
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-10-24 23:45:55 -0500

    Making final changes

--- 0x00-unity-audio/Assets/Scripts/MainMenu.cs
+++ 0x00-unity-audio/Assets/Scripts/MainMenu.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer master;
     public void LevelSelect(int level)
     {
         SceneManager.LoadScene(level);
@@ -16,5 +19,20 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("QUIT");
         Application.Quit();
+    }
+    public void Start() {
+        master.SetFloat("bgmVol", LinearToDecibel(PlayerPrefs.GetFloat("BGMVolume")));
+        master.SetFloat("sfxVol", LinearToDecibel(PlayerPrefs.GetFloat("SFXVolume")));
+    }
+
+    private float LinearToDecibel(float linear) {
+        float dB;
+
+        if (linear != 0)
+            dB = 20.0f * Mathf.Log10(linear);
+        else
+            dB = -144.0f;
+
+        return dB;
     }
 }
--- 0x00-unity-audio/Assets/Scripts/OptionsMenu.cs
+++ 0x00-unity-audio/Assets/Scripts/OptionsMenu.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
     public bool isInverted;
     public Toggle inverted;
+    public Slider bgm;
+    public static float bgmSlider = 1f;
+    public Slider sfx;
+    public static float sfxSlider = 1f;
+    public AudioMixer master;
+ 
     public void Start()
     {
         if (PlayerPrefs.GetInt("Inverted") == 1)
@@ -20,13 +27,22 @@ public class OptionsMenu : MonoBehaviour
             inverted.isOn = false;
             isInverted = false;
         }
+        bgm.value = bgmSlider;
+        // PlayerPrefs.GetFloat("BGMVolume");
+        sfx.value = sfxSlider;
+        // PlayerPrefs.GetFloat("SFXVolume");
     }
     public void Back()
     {
+        SetBGMVol(PlayerPrefs.GetFloat("BGMVolume"));
+        SetSFXVol(PlayerPrefs.GetFloat("SFXVolume"));
         SceneManager.LoadScene(PlayerPrefs.GetInt("LastScene"));
     }
     public void Apply()
     {
+        PlayerPrefs.SetFloat("BGMVolume", bgm.value);
+        PlayerPrefs.SetFloat("SFXVolume", sfx.value);
+        SceneManager.LoadScene(PlayerPrefs.GetInt("LastScene"));
         if (inverted.isOn)
         {
             PlayerPrefs.SetInt("Inverted", 1);
@@ -35,6 +51,23 @@ public class OptionsMenu : MonoBehaviour
         {
             PlayerPrefs.SetInt("Inverted", 0);
         }
-        SceneManager.LoadScene(PlayerPrefs.GetInt("LastScene"));
+    }
+    public void SetBGMVol(float vol) {
+        bgmSlider = vol;
+        master.SetFloat("bgmVol", LinearToDecibel(vol));
+    }
+    public void SetSFXVol(float vol) {
+        sfxSlider = vol;
+        master.SetFloat("sfxVol", LinearToDecibel(vol));
+    }
+    private float LinearToDecibel(float linear) {
+        float dB;
+
+        if (linear != 0)
+            dB = 20.0f * Mathf.Log10(linear);
+        else
+            dB = -144.0f;
+
+        return dB;
     }
 }
